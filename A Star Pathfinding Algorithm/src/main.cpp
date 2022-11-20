c5512f836afb055dbac55f0e80894c338e2374a4
niranjan-gurung <gurung5130@gmail.com>
2022-11-20 15:08:31 +0000

    restructure

--- A Star Pathfinding Algorithm/src/main.cpp
+++ A Star Pathfinding Algorithm/src/main.cpp
@@ -13,8 +13,6 @@ static const int SCREEN_HEIGHT = 600;
 static const int mapWidth = 20;
 static const int mapHeight = 20;
 
-using Tile = sf::RectangleShape;
-
 bool algorithmStart = false;
 
 // mouse flags:
@@ -22,6 +20,66 @@ bool mouseLeftDown = false;
 bool mouseRightDown = false;
 bool startKeyDown = false;
 bool endKeyDown = false;
+
+struct Node;
+
+using Tile = sf::RectangleShape;
+
+Node* startNode = nullptr;
+Node* endNode = nullptr;
+
+/* Forward Declarations: */
+// Inits:
+void InitGridConnections(std::vector<Node>& nodes);
+void HandleTileClick(
+    std::vector<Node>& nodes, 
+    const sf::Vector2f& mpos, 
+    const sf::Color& colour);
+
+// Main Algorithm:
+void AStarAlgorithm();
+
+// A* Path:
+void RetracePath(Node* start, Node* end);
+
+// Main loop:
+void UpdateImGuiContext(
+    std::vector<Node>& nodes);
+void Update(
+    sf::RenderWindow& window, 
+    std::vector<Node>& nodes, 
+    sf::Vector2f& mpos, 
+    sf::Clock& dt);
+void Render(
+    sf::RenderWindow& window, 
+    std::vector<Node>& nodes);
+
+int main()
+{
+    // sfml + imgui window inits:
+    sf::RenderWindow window(
+        sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT),
+        "A* Pathfinding Algorithm");
+    ImGui::SFML::Init(window);
+
+    // grid of nodes:
+    std::vector<Node> nodes(mapWidth * mapHeight);
+
+    // mouse coords:
+    sf::Vector2f mpos{};
+
+    // setup tile grid:
+    InitGridConnections(nodes);
+
+    sf::Clock dt;
+    while (window.isOpen())
+    {
+        Update(window, nodes, mpos, dt);
+        UpdateImGuiContext(nodes);  /* imgui menu: */
+        Render(window, nodes);
+    }
+    ImGui::SFML::Shutdown();
+}
 
 struct Node
 {
@@ -39,14 +97,14 @@ struct Node
     }
 
     // individual grid squares:
-    Tile tile;  
+    Tile tile;
 
     // track current node's parent:
     Node* parent = nullptr;
 
     // surrounding nodes for any given node:
     std::vector<Node*> neighbours;
-    
+
     // walls:
     bool obstacle = false;
     bool visited = false;
@@ -60,13 +118,10 @@ struct Node
     auto GetTilePosition()
     {
         return sf::Vector2f(
-            tile.getPosition().x, 
+            tile.getPosition().x,
             tile.getPosition().y);
     }
 };
-
-Node* startNode = nullptr;
-Node* endNode = nullptr;
 
 void InitGridConnections(std::vector<Node>& nodes)
 {
@@ -81,9 +136,9 @@ void InitGridConnections(std::vector<Node>& nodes)
             nodes[x + mapWidth * y].tile
                 .setOrigin(
                     sf::Vector2f(
-                        nodes[x + mapWidth * y].tile.getPosition().x + 
-                        (nodes[x + mapWidth * y].tile.getGlobalBounds().width / 2), 
-                        nodes[x + mapWidth * y].tile.getPosition().y + 
+                        nodes[x + mapWidth * y].tile.getPosition().x +
+                        (nodes[x + mapWidth * y].tile.getGlobalBounds().width / 2),
+                        nodes[x + mapWidth * y].tile.getPosition().y +
                         (nodes[x + mapWidth * y].tile.getGlobalBounds().height / 2)));
 
             nodes[x + mapWidth * y].tile
@@ -102,43 +157,43 @@ void InitGridConnections(std::vector<Node>& nodes)
             // top node
             if (y > 0)
                 nodes[x + mapWidth * y]
-                    .neighbours.push_back(&nodes[x + mapWidth * (y - 1)]);
+                .neighbours.push_back(&nodes[x + mapWidth * (y - 1)]);
             // right node
             if (x < mapWidth - 1)
                 nodes[x + mapWidth * y]
-                    .neighbours.push_back(&nodes[(x + 1) + mapWidth * y]);
+                .neighbours.push_back(&nodes[(x + 1) + mapWidth * y]);
             // bottom node
             if (y < mapHeight - 1)
                 nodes[x + mapWidth * y]
-                    .neighbours.push_back(&nodes[x + mapWidth * (y + 1)]);
+                .neighbours.push_back(&nodes[x + mapWidth * (y + 1)]);
             // left node
             if (x > 0)
                 nodes[x + mapWidth * y]
-                    .neighbours.push_back(&nodes[(x - 1) + mapWidth * y]);
+                .neighbours.push_back(&nodes[(x - 1) + mapWidth * y]);
 
             // diagonal connections:
             // top left
             if (y > 0 && x > 0)
                 nodes[x + mapWidth * y]
-                    .neighbours.push_back(&nodes[(x - 1) + mapWidth * (y - 1)]);
+                .neighbours.push_back(&nodes[(x - 1) + mapWidth * (y - 1)]);
             // top right
             if (y > 0 && x < mapWidth - 1)
                 nodes[x + mapWidth * y]
-                    .neighbours.push_back(&nodes[(x + 1) + mapWidth * (y - 1)]);
+                .neighbours.push_back(&nodes[(x + 1) + mapWidth * (y - 1)]);
             // bottom right
             if (y < mapHeight - 1 && x < mapWidth - 1)
                 nodes[x + mapWidth * y]
-                    .neighbours.push_back(&nodes[(x + 1) + mapWidth * (y + 1)]);
+                .neighbours.push_back(&nodes[(x + 1) + mapWidth * (y + 1)]);
             // bottom left
             if (y < mapHeight - 1 && x > 0)
                 nodes[x + mapWidth * y]
-                    .neighbours.push_back(&nodes[(x - 1) + mapWidth * (y + 1)]);
+                .neighbours.push_back(&nodes[(x - 1) + mapWidth * (y + 1)]);
         }
     }
 }
 
 void HandleTileClick(
-    std::vector<Node>& nodes, 
+    std::vector<Node>& nodes,
     const sf::Vector2f& mpos,
     const sf::Color& colour = sf::Color::Black)
 {
@@ -150,7 +205,7 @@ void HandleTileClick(
         {
             // set start node:
             if (sf::Keyboard
-                    ::isKeyPressed(sf::Keyboard::S))
+                ::isKeyPressed(sf::Keyboard::S))
             {
                 row.tile
                     .setFillColor(sf::Color::Green);
@@ -158,7 +213,7 @@ void HandleTileClick(
             }
             // set end node:
             else if (sf::Keyboard
-                    ::isKeyPressed(sf::Keyboard::E))
+                ::isKeyPressed(sf::Keyboard::E))
             {
                 row.tile
                     .setFillColor(sf::Color::Red);
@@ -166,7 +221,7 @@ void HandleTileClick(
             }
             // remove walls:
             else if (sf::Mouse
-                    ::isButtonPressed(sf::Mouse::Right))
+                ::isButtonPressed(sf::Mouse::Right))
             {
                 row.tile
                     .setFillColor(sf::Color::White);
@@ -190,7 +245,7 @@ void RetracePath(Node* start, Node* end)
     while (tracker->parent != nullptr) {
         // continue to update tracker to current node's parent until start node is reached:
         tracker = tracker->parent;
-        
+
         // only colour in path:
         if (tracker != startNode)
             tracker->tile.setFillColor(sf::Color::Yellow);
@@ -198,7 +253,7 @@ void RetracePath(Node* start, Node* end)
 }
 
 /* Main Algorithm : */
-void AStarAlgorithm() 
+void AStarAlgorithm()
 {
     /* returns distance between any two given tiles.
      * used for calculating g and h costs
@@ -223,7 +278,7 @@ void AStarAlgorithm()
         // find lowest fcost node from openlist:
         for (int i = 1; i < openList.size(); i++)
         {
-            if (openList[i]->fcost <= currentNode->fcost 
+            if (openList[i]->fcost <= currentNode->fcost
                 || openList[i]->hcost < currentNode->hcost)
             {
                 currentNode = openList[i];
@@ -259,173 +314,159 @@ void AStarAlgorithm()
                 continue;
             }
 
-            float costToMove = 
+            float costToMove =
                 currentNode->gcost + distance(currentNode, currentNeighbour);
 
             if (!(std::find(
-                    openList.begin(),
-                    openList.end(),
-                    currentNeighbour) != openList.end()))
+                openList.begin(),
+                openList.end(),
+                currentNeighbour) != openList.end()))
                 openList.push_back(currentNeighbour);
             else if (costToMove >= currentNeighbour->gcost)
                 continue;
-            
+
             currentNeighbour->parent = currentNode;
             currentNeighbour->gcost = costToMove;
             currentNeighbour->hcost = distance(currentNeighbour, endNode);
             currentNeighbour->fcost = currentNeighbour->gcost + currentNeighbour->hcost;
-            
-            std::cout 
-                << "x: " << currentNeighbour->tile.getPosition().x 
-                << " y: " << currentNeighbour->tile.getPosition().y 
+
+            std::cout
+                << "x: " << currentNeighbour->tile.getPosition().x
+                << " y: " << currentNeighbour->tile.getPosition().y
                 << " fcost: " << currentNeighbour->fcost << std::endl;
         }
     }
 }
 
-int main()
+void UpdateImGuiContext(std::vector<Node>& nodes)
 {
-    // sfml + imgui window inits:
-    sf::RenderWindow window(
-        sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT),
-        "A* Pathfinding Algorithm");
-    ImGui::SFML::Init(window);
+    ImGui::Begin("Menu");
 
-    // grid of nodes:
-    std::vector<Node> nodes(mapWidth * mapHeight);
+    if (ImGui::Button("visualise"))
+        algorithmStart = true;
 
-    // current mouse coordinates:
-    sf::Vector2f mpos;
-
-    // setup tile grid:
-    InitGridConnections(nodes);
-
-    sf::Clock dt;
-    while (window.isOpen())
+    if (ImGui::Button("clear"))
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        for (auto& row : nodes)
         {
-            ImGui::SFML::ProcessEvent(event);
-            switch (event.type)
-            {
+            startNode = endNode = nullptr;
+            row.parent = nullptr;
+            row.obstacle = false;
+            row.gcost = row.hcost = row.fcost = 0;
+            row.tile
+                .setFillColor(sf::Color::White);
+        }
+    }
+    ImGui::End();
+}
+
+void Update(sf::RenderWindow& window, std::vector<Node>& nodes, sf::Vector2f& mpos, sf::Clock& dt)
+{
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        ImGui::SFML::ProcessEvent(event);
+        switch (event.type)
+        {
             // close window:
-            case sf::Event::Closed:
-                window.close();
-                break;
+        case sf::Event::Closed:
+            window.close();
+            break;
 
             // key events for choosing start + end tile:
-            case sf::Event::KeyPressed:
-                switch (event.key.code)
-                {
-                case sf::Keyboard::S:
-                    startKeyDown = true;
-                    break;
-
-                case sf::Keyboard::E:
-                    endKeyDown = true;
-                    break;
-                }
+        case sf::Event::KeyPressed:
+            switch (event.key.code)
+            {
+            case sf::Keyboard::S:
+                startKeyDown = true;
                 break;
 
-            case sf::Event::KeyReleased:
-                switch (event.key.code)
-                {
-                case sf::Keyboard::S:
-                    startKeyDown = false;
-                    break;
-
-                case sf::Keyboard::E:
-                    endKeyDown = false;
-                    break;
-                }
+            case sf::Keyboard::E:
+                endKeyDown = true;
                 break;
+            }
+            break;
+
+        case sf::Event::KeyReleased:
+            switch (event.key.code)
+            {
+            case sf::Keyboard::S:
+                startKeyDown = false;
+                break;
+
+            case sf::Keyboard::E:
+                endKeyDown = false;
+                break;
+            }
+            break;
 
             // Mouse events:
-            case sf::Event::MouseButtonPressed:
-                switch (event.mouseButton.button)
-                {
-                case sf::Mouse::Left:
-                    mouseLeftDown = true;
-                    break;
-                
-                case sf::Mouse::Right:
-                    mouseRightDown = true;
-                    break;
-                }
-                break;
-            
-            case sf::Event::MouseButtonReleased:
-                switch (event.mouseButton.button)
-                {
-                case sf::Mouse::Left:
-                    mouseLeftDown = false;
-                    break;
-
-                case sf::Mouse::Right:
-                    mouseRightDown = false;
-                    break;
-                }
-                break;
-            
-            case sf::Event::MouseMoved:
-                // current mouse position:
-                mpos = window
-                    .mapPixelToCoords(
-                        sf::Mouse::getPosition(window));
-                break;
-            }
-        }
-
-        /* Update */
-        ImGui::SFML::Update(
-            window, dt.restart());
-
-        // start/end node:
-        if (mouseLeftDown && 
-            (startKeyDown || endKeyDown))
-            HandleTileClick(nodes, mpos);
-
-        // draw walls:
-        if (mouseLeftDown)
-            HandleTileClick(nodes, mpos);
-
-        // remove walls:
-        if (mouseRightDown)
-            HandleTileClick(nodes, mpos);
-
-        // A* visualisation..
-        if (algorithmStart)
-            AStarAlgorithm();
-
-        /* imgui menu: */
-        ImGui::Begin("Menu");
-
-        if (ImGui::Button("visualise"))
-            algorithmStart = true;
-
-        if (ImGui::Button("clear"))
-            for (auto& row : nodes)
+        case sf::Event::MouseButtonPressed:
+            switch (event.mouseButton.button)
             {
-                startNode = endNode = nullptr;
-                row.parent = nullptr;
-                row.obstacle = false;
-                row.gcost = row.hcost = row.fcost = 0;
-                row.tile
-                    .setFillColor(sf::Color::White);
+            case sf::Mouse::Left:
+                mouseLeftDown = true;
+                break;
+
+            case sf::Mouse::Right:
+                mouseRightDown = true;
+                break;
             }
+            break;
 
-        ImGui::End();
+        case sf::Event::MouseButtonReleased:
+            switch (event.mouseButton.button)
+            {
+            case sf::Mouse::Left:
+                mouseLeftDown = false;
+                break;
 
-        /* Render */
-        window.clear(sf::Color::Blue);
-        
-        // display grid:
-        for (const auto& row : nodes)
-            window.draw(row.tile);
-     
-        ImGui::SFML::Render(window);
-        window.display();
+            case sf::Mouse::Right:
+                mouseRightDown = false;
+                break;
+            }
+            break;
+
+        case sf::Event::MouseMoved:
+            // current mouse position:
+            mpos = window
+                .mapPixelToCoords(
+                    sf::Mouse::getPosition(window));
+            break;
+        }
     }
-    ImGui::SFML::Shutdown();
+
+    /* Update */
+    ImGui::SFML::Update(
+        window, dt.restart());
+
+    // start/end node:
+    if (mouseLeftDown &&
+        (startKeyDown || endKeyDown))
+        HandleTileClick(nodes, mpos);
+
+    // draw walls:
+    if (mouseLeftDown)
+        HandleTileClick(nodes, mpos);
+
+    // remove walls:
+    if (mouseRightDown)
+        HandleTileClick(nodes, mpos);
+
+    // A* visualisation..
+    if (algorithmStart)
+        AStarAlgorithm();
+}
+
+void Render(sf::RenderWindow& window, std::vector<Node>& nodes)
+{
+    /* Render */
+    window.clear(sf::Color::Blue);
+
+    // display grid:
+    for (const auto& row : nodes)
+        window.draw(row.tile);
+
+    ImGui::SFML::Render(window);
+    window.display();
 }
